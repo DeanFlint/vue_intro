0f989047531261d6aaf686fc63f726874e03ab20
flintd2 <dean.flint@ii.co.uk>
2018-06-27 09:31:08 +0100

    Template index.html setup

--- gulpfile.js
+++ gulpfile.js
@@ -11,7 +11,7 @@ gulp.task('sass', function(){
 
 // Move JS Files to src/js
 gulp.task('js', function(){
-	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js'])
+	return gulp.src(['node_modules/bootstrap/dist/js/bootstrap.min.js', 'node_modules/jquery/dist/jquery.min.js', 'node_modules/popper.js/dist/umd/popper.min.js', 'node_modules/vue/dist/vue.min.js'])
 		.pipe(gulp.dest("static/js"))
 });
 
